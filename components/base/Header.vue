da8e197153491cda34b5a571ebc694908fd8d187
losthope <arsen.melnuchyk@gmail.com>
2024-09-14 01:45:41 +0300

    update header component

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -1,29 +1,39 @@
 <template>
     <header>
-        <div class="flex max-sm:flex-col justify-center bg-blue-700 text-white p-5 2xl:space-x-10 xl:space-x-16 md:space-x-20">
-            <div class="max-sm:p-3">
+        <div
+            class="flex justify-between items-center bg-blue-700 text-white p-5 max-sm:flex-col max-sm:space-y-5">
+            <div class="max-sm:flex max-sm:flex-col max-sm:p-2 max-sm:self-start">
+
+                <NuxtLink to="/" class="rounded-xl p-2 hover:bg-black transition ease-in-out duration-500">
+                    Home
+                </NuxtLink>
+
+            </div>
+
+            <div class="max-sm:flex max-sm:flex-col max-sm:p-3 max-sm:self-start">
+                
                 <NuxtLink to="/quizes" class="rounded-xl p-2 hover:bg-black transition ease-in-out duration-500">
                     Quizes
                 </NuxtLink>
-            </div>
 
-            <div class="max-sm:p-3">
                 <NuxtLink to="/articles" class="rounded-xl p-2 hover:bg-black transition ease-in-out duration-500">
                     Articles
                 </NuxtLink>
+
             </div>
 
-            <div class="max-sm:p-3">
+            <div class="max-sm:flex max-sm:flex-col max-sm:p-3 max-sm:self-start">
+
                 <NuxtLink to="/register" class="rounded-xl p-2 hover:bg-black transition ease-in-out duration-500">
                     Register
                 </NuxtLink>
-            </div>
 
-            <div class="max-sm:p-3">
                 <NuxtLink to="/login" class="rounded-xl p-2 hover:bg-black transition ease-in-out duration-500">
-                Login
+                    Login
                 </NuxtLink>
+
             </div>
+            
         </div>
     </header>
 </template>
