d20f6f3e2c010e91e4a77e6dcbadbb4a4cc0940e
losthope <arsen.melnuchyk@gmail.com>
2024-09-17 01:04:05 +0300

    added: custom props to the quiz cards component

--- components/quiz/Card.vue
+++ components/quiz/Card.vue
@@ -1,6 +1,18 @@
+<script lang="ts">
+
+export default {
+    props: {
+        title: String,
+        imgSrc: String
+    }
+}
+
+</script>
+
 <template>
-    <div class="bg-orange-200 2xl:w-96 2xl:h-96 xl:h-80 xl:w-80 rounded-2xl flex justify-between flex-col items-center hover:scale-105 duration-500">
-        <NuxtImg />
-        <label>1</label>
+    <div
+        class="bg-orange-200 2xl:w-96 2xl:h-96 xl:h-80 xl:w-80 rounded-2xl flex justify-between flex-col items-center hover:scale-105 duration-500 hover:cursor-pointer">
+        <NuxtImg :src="imgSrc" />
+        <label>{{ title }}</label>
     </div>
 </template>
