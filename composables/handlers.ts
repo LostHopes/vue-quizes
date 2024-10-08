a25097f422445dbc8dce47a72d54b1667da5b0eb
losthope <arsen.melnuchyk@gmail.com>
2024-10-08 23:43:36 +0300

    updated package-lock, changed prisma scheme title, init search composables

--- composables/handlers.ts
+++ composables/handlers.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-export async function handleRegister () {
-
+export async function handleRegister() {
 }
 
-export async function handleLogin () {
-    
+export async function handleLogin() {
+    const user = await prisma.user.findFirst({
+    });
 }
---
+++ composables/search.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function findByTitle(title: string) {}
+
+export async function findByDesc(description: string) {}
+
+export async function findByPopularity(popularity: string) {}
+
